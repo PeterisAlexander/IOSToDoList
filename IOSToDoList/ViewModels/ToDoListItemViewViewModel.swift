a8b6924a73f9c82a005819e531a4f2dc512bd0dd
peterisalexander <pad7887@gmail.com>
2024-08-25 19:55:27 +0200

    End app

--- IOSToDoList/ViewModels/ToDoListItemViewViewModel.swift
+++ IOSToDoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -4,9 +4,26 @@
 //
 //  Created by Pierre Alexandre D. on 24/08/2024.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class ToDoListItemViewViewModel: ObservableObject {
     init() {}
+    
+    func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+    }
 }
